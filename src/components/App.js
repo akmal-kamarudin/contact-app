f27b87b88c3b50fb97384e5d805e04206a391d83
Akmal Kamarudin <akmal.kamarudin19@gmail.com>
2023-03-22 00:48:41 +0800

    style: installed semantic ui | chore: added a few components for the app.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,19 @@
+import React from "react";
 import "./App.css";
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <Header />
+      <div className="ui container">
+        <AddContact />
+        <ContactList />
+      </div>
+    </>
+  );
 }
 
 export default App;
