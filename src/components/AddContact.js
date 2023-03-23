829d638007d3ac78932d8cf78194d4729fc8a4cb
Akmal Kamarudin <akmal.kamarudin19@gmail.com>
2023-03-24 01:02:45 +0800

    chore: make some changes to App.js, stored the data in localStorage. UseEffect is used to store the state of the data(contacts) are rendered when there is data and will return an empty array if there none(null)

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -12,8 +12,9 @@ class AddContact extends React.Component {
       alert("All the fields are mandatory!");
       return;
     }
-
-    console.log(this.state);
+    this.props.addContactHandler(this.state);
+    this.setState({ name: "", email: "" });
+    // console.log(this.state);
   };
 
   render() {
