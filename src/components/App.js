1d2937f3b3c7ec5a69b57780ef912feba62e68f5
Akmal Kamarudin <akmal.kamarudin19@gmail.com>
2023-03-23 23:16:57 +0800

    chore: deleted the static object on App.js and put the empty object in AddContact.js. Then, use a state object to capture the inputs inside the form when added a new contact

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
-  const contacts = [
-    {
-      id: 1,
-      name: "Akmal",
-      email: "akmal@gmail.com",
-    },
-    {
-      id: 1,
-      name: "Amirah",
-      email: "amirah@gmail.com",
-    },
-  ];
+  const [contacts, setContacts] = useState([]);
+  // const contacts = [
+  //   {
+  //     id: 1,
+  //     name: "Akmal",
+  //     email: "akmal@gmail.com",
+  //   },
+  //   {
+  //     id: 1,
+  //     name: "Amirah",
+  //     email: "amirah@gmail.com",
+  //   },
+  // ];
 
   return (
     <>
