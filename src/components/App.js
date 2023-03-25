53a63b77a4f95ac1ee616df4063202861c530088
Akmal Kamarudin <akmal.kamarudin19@gmail.com>
2023-03-25 16:00:52 +0800

    build: installed uuid for handling the keys of each item, | feat: system can now save each sets of data in the localStorage

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { v4 as uuid } from "uuid";
 import "./App.css";
 import Header from "./Header";
 import AddContact from "./AddContact";
@@ -11,14 +12,9 @@ function App() {
   );
 
   const addContactHandler = (contact) => {
-    console.log(contact);
+    // console.log(contact);
     setContacts([...contacts, contact]);
   };
-
-  // useEffect(() => {
-  //   const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-  //   if (retriveContacts) setContacts(retriveContacts);
-  // }, []);
 
   useEffect(() => {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
